8dae035b5141d33bda653b8d35776903c89fd34e
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-12-16 17:34:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char array1[10][20];
+    char array2[20];
+    int numero=0;
+    for(int i=0; i<10; i++){
+        for(int k=0; k<20; k++){
+            array1[i][k]=' ';
+            array2[k]=' ';
+        }
+    }
+    for(int i=0; i<10; i++){
+        cin >> array1[i];
+    }
+    cin>>array2;
+    if(posizione(array1, array2, numero)){
+        cout << numero;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
